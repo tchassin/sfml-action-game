cf049b0752d1736cf840f9a8ef1bf5ec1b37ac9b
tchassin <thomaschassin@hotmail.fr>
2023-08-05 14:37:00 +0900

    Add primitive type definitions based on SFML types

---
+++ Source/Types.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SFML/Config.hpp>
+
+using i8 = sf::Int8;
+using u8 = sf::Uint8;
+
+using i16 = sf::Int16;
+using u16 = sf::Uint16;
+
+using i32 = sf::Int32;
+using u32 = sf::Uint32;
+
+using i64 = sf::Int64;
+using u64 = sf::Uint64;
+
+using f32 = float;
+using f64 = double;

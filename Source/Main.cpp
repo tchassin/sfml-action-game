c62a7c3cb06b6c284ba261ff1a4489c2791ad40b
tchassin <thomaschassin@hotmail.fr>
2023-08-05 14:44:47 +0900

    Added basic player movement to test input handling
    # Conflicts:
    #	Source/Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -5,6 +5,7 @@
 
 #include "Game.h"
 #include "GameObject.h"
+#include "PlayerInput.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
@@ -16,7 +17,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
     UNREFERENCED_PARAMETER(nCmdShow);
 
+    sf::Clock clock;
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    window.setFramerateLimit(60);
 
     Game game;
     GameObject gameObject(&game, "Knight", sf::IntRect(0, 0, 24, 24), sf::Vector2f(12, 12));
@@ -30,6 +34,24 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        PlayerInput::update();
+
+        sf::Time deltaTime = clock.getElapsedTime();
+        clock.restart();
+
+        const float speed = 64.0f * deltaTime.asSeconds();
+
+        if (PlayerInput::isButtonDown(PlayerInput::Button::Left))
+            gameObject.getSprite().move(sf::Vector2f(-speed, 0));
+
+        if (PlayerInput::isButtonDown(PlayerInput::Button::Right))
+            gameObject.getSprite().move(sf::Vector2f(speed, 0));
+
+        if (PlayerInput::isButtonPressed(PlayerInput::Button::Left))
+            gameObject.getSprite().setScale(sf::Vector2f(-1, 1));
+        if (PlayerInput::isButtonPressed(PlayerInput::Button::Right))
+            gameObject.getSprite().setScale(sf::Vector2f(1, 1));
 
         window.clear();
         window.draw(gameObject);

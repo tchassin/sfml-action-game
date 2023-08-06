9ca11d827af87b7c012f705ae9e3349c00e61987
tchassin <thomaschassin@hotmail.fr>
2023-08-06 15:17:36 +0900

    Create a class to represent game characters

---
+++ Source/Character.cpp
@@ -0,0 +1,61 @@
+#include "Character.h"
+
+namespace
+{
+    constexpr f32 GRAVITY = 9.81f * 100.0f * 2.0f;
+    constexpr f32 GROUND_HEIGHT = 200.0f;
+}
+
+Character::Character(Game* owner, const std::string& spritePath, sf::IntRect spriteRect, sf::Vector2f pivot)
+    : base(owner, spritePath, spriteRect, pivot)
+{
+}
+
+Character::~Character()
+{
+}
+
+void Character::move(sf::Vector2f offset, sf::Time deltaTime)
+{
+    // Flip sprite to match current direction
+    if (offset.x < 0.0f && !isFlipped())
+        getTransform().setScale(-1, 1);
+    else if (offset.x > 0.0f && isFlipped())
+        getTransform().setScale(1, 1);
+
+    const sf::Vector2f position = getTransform().getPosition();
+    const f32 maxJumpDuration = m_maxJumpHeight / m_jumpSpeed - m_jumpSpeed / GRAVITY;
+
+    // Handle jumping and falling
+    offset.x *= isGrounded() ? m_runningSpeed : m_horizontalAirSpeed;
+    if (!isFalling() && offset.y < 0.0f)
+    {
+        offset.y = -m_jumpSpeed;
+        if (m_airTime >= maxJumpDuration)
+            offset.y += GRAVITY * deltaTime.asSeconds();
+    }
+    else
+    {
+        offset.y = std::min(m_maxFallSpeed, m_verticalVelocity + deltaTime.asSeconds() * GRAVITY);
+    }
+
+    m_verticalVelocity = offset.y;
+
+    // Move the character while staying on screen
+    sf::Vector2f targetPosition = position + offset * deltaTime.asSeconds();
+    targetPosition.x = std::clamp(targetPosition.x, 0.0f, 200.0f);
+    targetPosition.y = std::clamp(targetPosition.y, 0.0f, GROUND_HEIGHT);
+    getTransform().setPosition(targetPosition);
+
+    // Check if the character is in the air
+    if (abs(getTransform().getPosition().y - GROUND_HEIGHT) < 1.0f)
+    {
+        m_airTime = 0.0f;
+        m_verticalVelocity = 0.0f;
+    }
+    else
+    {
+        m_airTime += deltaTime.asSeconds();
+    }
+}
+
---
+++ Source/Character.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "GameObject.h"
+
+#include <SFML/System/Time.hpp>
+
+#include "Types.h"
+
+class Character : public GameObject
+{
+    using base = GameObject;
+
+public:
+    Character(Game* owner, const std::string& spritePath, sf::IntRect spriteRect, sf::Vector2f pivot);
+    virtual ~Character();
+
+public:
+    void move(sf::Vector2f offset, sf::Time deltaTime);
+
+    bool isFlipped() const { return getTransform().getScale().x < 0.0f; }
+    bool isGrounded() const { return m_airTime == 0.0f; }
+    bool isFalling() const { return !isGrounded() && m_verticalVelocity > -m_jumpSpeed; }
+
+    sf::Vector2f getForwardDirection() const { return sf::Vector2f(isFlipped() ? -1 : 1, 0); }
+
+    f32 getRunningSpeed() const { return m_runningSpeed; }
+    void setRunningSpeed(f32 speed) { m_runningSpeed = speed; }
+
+    f32 getHorizontalAirSpeed() const { return m_horizontalAirSpeed; }
+    void setHorizontalAirSpeed(f32 speed) { m_horizontalAirSpeed = speed; }
+
+    f32 getJumpSpeed() const { return m_jumpSpeed; }
+    void setJumpSpeed(f32 speed) { m_jumpSpeed = speed; }
+
+    f32 getFallSpeed() const { return m_maxFallSpeed; }
+    void setFallSpeed(f32 speed) { m_maxFallSpeed = speed; }
+
+    f32 getJumpHeight() const { return m_maxJumpHeight; }
+    void setJumpHeight(f32 height) { m_maxJumpHeight = height; }
+
+private:
+    f32 m_airTime = 0.0f;
+    f32 m_runningSpeed = 0.0f;
+    f32 m_horizontalAirSpeed = 0.0f;
+    f32 m_jumpSpeed = 0.0f;
+    f32 m_verticalVelocity = 0.0f;
+    f32 m_maxFallSpeed = 0.0f;
+    f32 m_maxJumpHeight = 0.0f;
+};
+
